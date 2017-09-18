3b32429e0217a9812bc044d908f693d19e26b183
ChangJoo Park <pcjpcj2@gmail.com>
2017-09-18 22:48:15 +0900

    Update port for heroku

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ app.get('/', viewController);
 const issues = require('./routers/issues')
 app.use('/api/issues', issues)
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
 })
