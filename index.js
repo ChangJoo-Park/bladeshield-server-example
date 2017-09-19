4e35958a94c38af2f6eccdc2ee3decab6f1c07d0
ChangJoo Park <changjoo.park@partner.so>
2017-09-19 09:40:32 +0900

    Allow CORS

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express')
 const router = express.Router()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-
+app.use(cors())
 // Database
 const db = mongoose.connection
 db.on('error', console.error)
