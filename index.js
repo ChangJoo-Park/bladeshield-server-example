c8e25a6e1107aaec874e063b194a35fe4170ce69
ChangJoo Park <changjoo.park@partner.so>
2017-09-19 10:39:33 +0900

    Add v-cloak, fix listen message

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ app.get('/', viewController);
 const issues = require('./routers/issues')
 app.use('/api/issues', issues)
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Example app listening on port 3000!')
+const port = process.env.PORT || 3000
+app.listen(port, function () {
+  console.log('Example app listening on port ', port)
 })
